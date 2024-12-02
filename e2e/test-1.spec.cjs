68d4a73778b48854d2a7d4437e37b8de6b9c1880
Noddan84 <146703809+Noddan84@users.noreply.github.com>
2024-12-02 13:12:34 +0100

    update
    flera tester

--- e2e/test-1.spec.cjs
+++ e2e/test-1.spec.cjs
@@ -1,7 +1,3 @@
-// 1. Nyfiken förstagångs-användare - klickar runt för att se vad som finns
-
-module.exports = { curiousUser };
-
 async function curiousUser(page) {
   await page.goto('http://localhost:4000/kategori/frukt-och-gront');
   await page.locator('div').filter({ hasText: /^Mejeri, ost & ägg\+$/ }).getByRole('button').click();
@@ -16,4 +12,79 @@ async function curiousUser(page) {
   await page.getByRole('link', { name: 'Texmex' }).click();
   await page.getByRole('link', { name: 'Kryddmix' }).click();
   await page.getByLabel('Sortera:PopulärastA – ÖÖ –').selectOption('name-asc');
+}
+
+async function knowerUser(page) {
+  await page.goto('http://localhost:4000/kategori');
+  await page.locator('div').filter({ hasText: /^Tobak\+$/ }).getByRole('button').click();
+  await page.locator('div').filter({ hasText: /^Snus\+$/ }).getByRole('button').click();
+  await page.getByRole('link', { name: 'Snus stock' }).click();
+  await page.getByRole('heading', { name: 'General Lös Lössnus' }).click();
+  await page.locator('html').click();
+  await page.goto('http://localhost:4000/kategori/tobak/snus/snus-stock');
+  await page.getByLabel('Sortera:PopulärastA – ÖÖ –').selectOption('price-asc');
+  await page.getByRole('button', { name: '>' }).click();
+  await page.getByRole('button', { name: '>' }).click();
+  await page.getByRole('heading', { name: 'Brukssnus Lös Lössnus' }).click();
+}
+
+async function comparerUser(page) {
+  await page.goto('http://localhost:4000/kategori/frukt-och-gront');
+  await page.getByRole('link', { name: 'Apotek' }).click();
+  await page.getByRole('link', { name: 'Plåster & sårvård' }).click();
+  await page.getByLabel('Sortera:PopulärastA – ÖÖ –').selectOption('price-asc');
+  await page.getByRole('heading', { name: 'Idomin Salva Oparfymerad' }).click();
+  await page.getByRole('link', { name: 'Plåster & sårvård' }).click();
+  await page.getByRole('link', { name: 'Receptfria läkemedel' }).click();
+  await page.getByRole('link', { name: 'Plåster & sårvård' }).click();
+  await page.getByLabel('Sortera:PopulärastA – ÖÖ –').selectOption('compareprice-asc');
+  await page.getByRole('heading', { name: 'Idomin Salva Oparfymerad' }).click();
+}
+
+async function greedyUser(page) {
+  await page.goto('http://localhost:4000/kategori/frukt-och-gront');
+  await page.getByLabel('Sortera:PopulärastA – ÖÖ –').selectOption('price-asc');
+  await page.getByRole('link', { name: 'Kött, chark & fågel' }).click();
+  await page.getByRole('link', { name: 'Fågel', exact: true }).click();
+  await page.getByRole('link', { name: 'Färsk fågel' }).click();
+  await page.getByRole('link', { name: 'Fryst fågel' }).click();
+  await page.getByRole('link', { name: 'Kött', exact: true }).click();
+  await page.getByRole('link', { name: 'Fläsk' }).click();
+  await page.getByRole('link', { name: 'Färdiglagat & pannfärdigt' }).click();
+  await page.getByRole('link', { name: 'Nöt & kalv' }).click();
+  await page.getByRole('link', { name: 'Köttfärs' }).click();
+  await page.getByRole('link', { name: 'Lamm & vilt' }).click();
+  await page.getByRole('link', { name: 'Kassler' }).click();
+  await page.getByRole('link', { name: 'Inälvsmat' }).click();
+  await page.getByRole('link', { name: 'Korv' }).click();
+  await page.getByRole('link', { name: 'Falukorv' }).click();
+  await page.getByRole('link', { name: 'Färska korvar' }).click();
+  await page.getByRole('link', { name: 'Grill, kok- & kryddkorv' }).click();
+  await page.getByRole('link', { name: 'Varm-, Wienerkorv mm' }).click();
+  await page.getByRole('link', { name: 'Kött- & fläskkorv mm' }).click();
+  await page.getByRole('link', { name: 'Vegetarisk korv' }).click();
+  await page.getByRole('link', { name: 'Ölkorv' }).click();
+  await page.getByRole('link', { name: 'Fryst korv' }).click();
+  await page.locator('div').filter({ hasText: /^Chark\+$/ }).getByRole('button').click();
+  await page.getByRole('link', { name: 'Bacon & stekfläsk' }).click();
+  await page.getByRole('link', { name: 'Köttbullar & färsprodukter' }).click();
+  await page.getByRole('link', { name: 'Blodpudding' }).click();
+  await page.getByRole('link', { name: 'Övrigt rökt & rimmat' }).click();
+  await page.getByRole('link', { name: 'Pålägg' }).click();
+  await page.getByRole('link', { name: 'Skinka' }).click();
+  await page.getByRole('link', { name: 'Kokt Skinka' }).click();
+  await page.getByRole('link', { name: 'Färskost' }).click();
+  await page.getByRole('link', { name: 'Skinksmörgås' }).click();
+  await page.getByRole('link', { name: 'Färskost - Kallskuret' }).click();
+  await page.getByRole('link', { name: 'Kallskuret' }).click();
+  await page.getByRole('link', { name: 'Högtider & Julafton' }).click();
+  await page.getByRole('link', { name: 'Rökt lax' }).click();
+  await page.getByRole('link', { name: 'Skinka' }).click();
+}
+
+module.exports = {
+  curiousUser,
+  knowerUser,
+  comparerUser,
+  greedyUser
 };
